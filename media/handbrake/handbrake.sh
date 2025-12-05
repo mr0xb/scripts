1c58245cc5f19be2235e64e84eefe7db7129cedf
Steve B <mr0xb@DESKTOP-BT4OA5Q.localdomain>
2025-12-05 14:56:18 -0500

    .

--- media/handbrake/handbrake.sh
+++ media/handbrake/handbrake.sh
@@ -5,7 +5,16 @@ if [ -f "$file" ];
 then
 
 	output="${file##*/}"
-	/usr/bin/HandBrakeCLI --preset-import-gui preset.json -map-metadata 0 -E mp3 -B 128 -b 1600 -e x265 -x vbv-maxrate=1600 -x vbv-bufsize=2200 -X 1920 -Y 1080 --audio-lang-list "eng" --subtitle-lang-list "eng" --all-subtitles -i "${file}" -o "${output%.*}.mkv"	
+	/usr/bin/HandBrakeCLI \
+		--preset-import-gui preset.json \
+		-map-metadata 0 -E mp3 -B 128 -b 1600 -e x265 \
+		-x vbv-maxrate=1600 -x vbv-bufsize=2200 \
+		-X 1920 -Y 1080 \
+		--audio-lang-list "eng" \
+		--subtitle-lang-list "eng" \
+		--all-subtitles \
+		-i "${file}" \
+		-o "${output%.*}.mkv"	
 	if [ $? -eq 0 ]
 	then
 		echo "would have moved $file to $file.old" >> log.txt
